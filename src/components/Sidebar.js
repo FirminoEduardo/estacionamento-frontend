8d0d64cc09f026e898a9a796f93d1c4d280db249
FirminoEduardo <dudu.lf@outlook.com>
2025-05-06 14:39:07 -0300

    Criação do sidebar.js

---
+++ src/components/Sidebar.js
@@ -0,0 +1,29 @@
+// Menu lateral para navegação entre páginas
+
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Sidebar = () => {
+  return (
+    <aside className="bg-gray-800 text-white w-64 p-4">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/clientes">Clientes</Link>
+          </li>
+          <li>
+            <Link to="/vagas">Vagas</Link>
+          </li>
+          <li>
+            <Link to="/pagamentos">Pagamentos</Link>
+          </li>
+          <li>
+            <Link to="/relatorios">Relatórios</Link>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+  );
+};
+
+export default Sidebar;
